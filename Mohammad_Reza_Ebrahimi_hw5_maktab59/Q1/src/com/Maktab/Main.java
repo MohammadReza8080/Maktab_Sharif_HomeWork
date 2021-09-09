cfcb987b6f5f4e040c33797cfde5b51e85f75421
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-09 10:24:57 +0430

    third initial

--- Mohammad_Reza_Ebrahimi_hw5_maktab59/Q1/src/com/Maktab/Main.java
+++ Mohammad_Reza_Ebrahimi_hw5_maktab59/Q1/src/com/Maktab/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int number = 18;
         isPrime(number, 2);
+        isPrime(number, 22);
     }
 
     public static void isPrime(int number, int i) {
